039e7beafc3ba682a880dd930e69f621eaf1246b
Sidre Nil Denge <dengesidrenil@gmail.com>
2024-07-17 11:28:16 +0300

    back-icon

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,32 +1,41 @@
 <template>
-  <div class="flex items-center justify-center mt-10">
-    <div class="flex container m-10">
-      <div
-        class="col-md-6 order-md-2 d-flex align-items-center justify-content-center"
-      >
-        <img
-          :src="movie.poster_path"
-          alt="Movie Poster"
-          class="img-fluid custom-img"
-        />
-      </div>
-      <div
-        class="col-md-6 order-md-1 d-flex flex-column justify-content-center ml-10"
-      >
-        <div>
-          <h1 class="text-5xl text-yellow-300 text-center">
-            {{ movie.title }}
-          </h1>
-          <p class="lead text-yellow-200 mt-10 text-justify">
-            {{ movie.overview }}
-          </p>
-          <div class="mt-4">
-            <p class="text-yellow-500 mt-10 text-end">
-              <strong>Release Date:</strong> {{ movie.release_date }}
+  <div class="relative">
+    <button
+      @click="goBack"
+      class="fixed mt-10 ml-5 top-2 left-4 bg-gray-500 text-white py-2 px-4 rounded-full"
+    >
+      <font-awesome-icon :icon="['fas', 'arrow-left']" />
+    </button>
+
+    <div class="flex items-center justify-center mt-10">
+      <div class="flex container m-10">
+        <div
+          class="col-md-6 order-md-2 d-flex align-items-center justify-content-center"
+        >
+          <img
+            :src="movie.poster_path"
+            alt="Movie Poster"
+            class="img-fluid custom-img"
+          />
+        </div>
+        <div
+          class="col-md-6 order-md-1 d-flex flex-column justify-content-center ml-10"
+        >
+          <div>
+            <h1 class="text-5xl text-yellow-300 text-center">
+              {{ movie.title }}
+            </h1>
+            <p class="lead text-yellow-200 mt-10 text-justify">
+              {{ movie.overview }}
             </p>
-            <p class="text-yellow-500 text-end mr-10">
-              <strong>Vote Average:</strong> {{ movie.vote_average }}
-            </p>
+            <div class="mt-4">
+              <p class="text-yellow-500 mt-10 text-end">
+                <strong>Release Date:</strong> {{ movie.release_date }}
+              </p>
+              <p class="text-yellow-500 text-end mr-10">
+                <strong>Vote Average:</strong> {{ movie.vote_average }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -36,11 +45,16 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { getMovieDetails } from "../api/movies";
 
 const route = useRoute();
+const router = useRouter();
+
 const movie = ref({});
+const goBack = () => {
+  router.back();
+};
 
 onMounted(async () => {
   try {
